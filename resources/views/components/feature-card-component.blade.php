eb9756df4c7888b60f76617109f5343e3dd12e93
Adiputra01 <madiputra0107@gmail.com>
2022-11-09 13:19:05 +0700

    merubah icon pada features

--- resources/views/components/feature-card-component.blade.php
+++ resources/views/components/feature-card-component.blade.php
@@ -2,6 +2,6 @@
 
 <div class="flex flex-col px-3 h-[235px] lg:h-[312px] justify-center items-center bg-primary-light">
     {{ $slot }}
-    <h3 class="text-mb-content-heading lg:text-content-heading text-dark font-bold">{{ $title }}</h3>
+    <h3 class="text-mb-content-heading lg:text-content-heading text-dark font-bold mt-3">{{ $title }}</h3>
     <p class="hidden lg:block text-gray px-4 text-center">{{ $description }}</p>
 </div>
