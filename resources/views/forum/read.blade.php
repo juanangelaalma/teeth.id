8214ff0bcfff809241db82da8e2d2a4d6dc4e322
Muhasibi <tanhanafsi@gmail.com>
2022-11-15 06:49:48 +0700

    slicing baca forum

---
+++ resources/views/forum/read.blade.php
@@ -0,0 +1,38 @@
+<x-forum-layout>
+    <h6 class="text-secondary text-mb-content-paragraph">Pernafasan</h6>
+    <h1 class="text-dark-gray text-content-heading">Batuk tidak sembuh-sembuh harus bagaimana?</h1>
+    <p class="text-light-gray text-mb-content-paragraph-small">3 Desember 2022</p>
+
+    {{-- QUESTION --}}
+    <div class="bg-primary-light rounded-lg px-3 py-2 flex justify-start items-center space-x-3 mt-4">
+        <div style="height: 40px; width: 40px;" class="rounded-full bg-[#A4D0C7] flex justify-center items-center">
+            <p class="text-white">D</p>
+        </div>
+        <p class="text-dark-gray text-mb-content-paragraph">Dodik Sudrajat</p>
+    </div>
+    <p class="text-content-paragraph text-dark-gray mt-3">
+        Dokter, wajah saya dari jaman remaja jerawatan terus hilang timbul dok. dan banyak muncul di area dahi dan dagu.
+        jerawat saya tipe jerawat nanah dan kecil-kecil dok. saya bingung pakai obat apalagi
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ut asperiores quis magni quos voluptates, qui
+        perferendis, reprehenderit aspernatur fugiat architecto deleniti illo natus dolore optio illum at atque
+        perspiciatis.
+    </p>
+
+    {{-- ANSWER --}}
+    <div class="border border-[#ECECEC] rounded-lg mt-4 p-4">
+        <p class="text-light-gray text-sm">Dijawab oleh:</p>
+        <div class="flex flex-row space-x-4 mt-4">
+            <div class="w-[50px] h-[50px] rounded-full overflow-hidden">
+                <img class="w-full h-full object-cover object-center"
+                    src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+                    alt="">
+            </div>
+            <div class="flex flex-col">
+                <h6 class="text-paragraph text-dark-gray">dr. Juan angela alma</h6>
+                {{-- <p class="text-mb-content-pragraph text-light-gray">{{ timestamp_to_date($article->created_at) }}</p> --}}
+                <p class="text-mb-content-pragraph text-light-gray">20 Desember 2022</p>
+            </div>
+        </div>
+        <div class="mt-4 text-dark-gray">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ducimus, minus alias dignissimos quisquam unde, possimus incidunt, eum ratione sed totam reprehenderit laboriosam? Facere illo assumenda ab possimus totam? Iusto voluptatem expedita odio. Dolorum vero, quidem eveniet aut consequuntur corrupti tempora distinctio voluptatem accusantium iure error nisi sequi adipisci dignissimos earum labore laboriosam architecto, ipsa quibusdam perspiciatis quam est impedit! Hic, rem neque nesciunt totam reiciendis voluptatem laboriosam illum quis laudantium iusto ut id repellendus, veniam vero quos dignissimos aspernatur voluptates. Hic atque possimus nisi pariatur, reprehenderit excepturi. Molestiae temporibus, doloribus assumenda voluptates ducimus illum obcaecati itaque dolorum nemo id similique dolor asperiores fuga, labore cum officia explicabo facere ratione sapiente aperiam mollitia. Delectus, ratione velit illo pariatur nihil itaque impedit deserunt similique voluptate non, quod vero reprehenderit et labore natus asperiores dolor! Minima error pariatur est veniam, dicta, ducimus officiis doloribus veritatis aut tempore magni explicabo voluptas consequatur facilis similique ex asperiores eveniet voluptatem, eligendi ea sunt dolores hic. Officia ut aut dignissimos soluta iste? Laudantium nulla consequuntur nam, voluptates dolores eum! Placeat est neque cum distinctio laudantium, animi adipisci facilis. Rerum corrupti quod fuga cupiditate beatae itaque nesciunt dicta. Libero dolore autem rerum distinctio, ducimus cumque officia quam!</div>
+    </div>
+</x-forum-layout>
