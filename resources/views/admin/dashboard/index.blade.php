6d6812a712380873a266fa17e17502889c4e4ced
Juan Alma <juanlah77@gmail.com>
2022-12-01 18:59:01 +0700

    change design

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -11,7 +11,7 @@
             <div class="flex flex-col lg:flex-row space-y-3 lg:space-y-0">
               <img class="h-auto lg:h-[130px]" src="{{ asset('assets/images/casual-life-3d-doctor-sitting-on-the-floor-with-books.png') }}" alt="">
               <div>
-                <p class="text-dark-gray text-md">Lihat beberapa permintaan verifikasi yang belum anda approve</p>
+                <p class="text-dark-gray text-sm mb-2">Lihat beberapa permintaan verifikasi yang belum anda approve</p>
                 <a href="" class="text-bold text-primary underline mt-3">Lihat</a>
               </div>
             </div>
@@ -24,7 +24,6 @@
                       <h1 class="text-primary text-section-paragraph">Selamat datang admin 🎉</h1>
                       <p class="text-content-paragraph text-dark-gray mb-6"><span class="text-dark font-semibold">Tetap semangat!!.</span> Masa depan adalah milik Anda yang telah menyiapkannya dari
                           hari ini</p>
-                      <x-main-button-link>Lihat Janji</x-main-button-link>
                   </div>
                       <img class="w-full lg:w-[200px]" src="{{ asset('assets/images/doctor-about.png') }}" alt="">
                   </div>
