67240cd60d64725db41521f5a4c39c7167d9a3d3
Juan Alma <juanlah77@gmail.com>
2022-12-16 18:38:44 +0700

    [Fix bugs] bug array key overflow

--- app/helpers.php
+++ app/helpers.php
@@ -6,7 +6,7 @@ function timestamp_to_date($timestamp) {
   $date = date('Y-m-d', strtotime($timestamp));
   $months = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
   $splittedDate = explode('-', $date);
-  return $splittedDate[2] . ' ' . $months[(int)$splittedDate[1]] . ' ' . $splittedDate[0];
+  return $splittedDate[2] . ' ' . $months[$splittedDate[1]-1] . ' ' . $splittedDate[0];
 }
 
 // function get_diff_timestap($timestamp) {
